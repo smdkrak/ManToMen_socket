4887f79ff4cdd7744986b3ca706eb73b177ec068
Ulegi <smwhite1128@naver.com>
2021-06-23 20:04:59 +0900

    First commit

---
+++ src/main/java/com/e2ic/mantomen/MantomenApplication.java
@@ -0,0 +1,13 @@
+package com.e2ic.mantomen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantomenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantomenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e2ic/mantomen/MantomenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2ic.mantomen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantomenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
